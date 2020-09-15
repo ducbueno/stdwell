e8234ceb6ddbc84c09a9e17912b9ce24588b6713
Jose Eduardo Bueno <ducbueno@gmail.com>
2020-09-15 14:37:32 -0300

    Updted script

--- matlab/plot_data.m
+++ matlab/plot_data.m
@@ -2,13 +2,15 @@ function plot_data(model)
     y = readmatrix(strcat('../data/real/', model,'/y.txt'));
     y_opencl = readmatrix(strcat('../data/real/', model,'/y_-opencl.txt'));
     y_cuda = readmatrix(strcat('../data/real/', model,'/y_-cuda.txt'));
-
-    figure;
+    
+    err_opencl = norm(y_cuda - y_opencl)/norm(y_cuda);
+    
+    figure; 
     subplot(1, 2, 1);
     plot(y_cuda - y);
+    title('CUDA');
     subplot(1, 2, 2);
     plot(y_opencl - y);
-
-    err_opencl = norm(y_cuda - y_opencl)/norm(y_cuda);
-    disp(err_opencl);
+    title('OpenCL');
+    sgtitle(strcat({'Model '}, upper(model), {', error = '}, num2str(err_opencl)));
 end
