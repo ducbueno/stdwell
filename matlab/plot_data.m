93936aa9db8219cc4cbe182ebf379a59777995fa
Jose Eduardo Bueno <ducbueno@gmail.com>
2020-09-22 15:50:12 -0300

    Updated matlab script

--- matlab/plot_data.m
+++ matlab/plot_data.m
@@ -1,16 +1,26 @@
 function plot_data(model)
-    y = readmatrix(strcat('../data/real/', model,'/y.txt'));
-    y_opencl = readmatrix(strcat('../data/real/', model,'/y_-opencl.txt'));
-    y_cuda = readmatrix(strcat('../data/real/', model,'/y_-cuda.txt'));
+    v = readmatrix(strcat('../data/real/', model,'/v.txt'));
+    v_opencl = readmatrix(strcat('../data/real/', model,'/v_-opencl.txt'));
+    v_cuda = readmatrix(strcat('../data/real/', model,'/v_-cuda.txt'));
     
-    err_opencl = norm(y_cuda - y_opencl)/norm(y_cuda);
+    t = readmatrix(strcat('../data/real/', model,'/t.txt'));
+    t_opencl = readmatrix(strcat('../data/real/', model,'/t_-opencl.txt'));
+    t_cuda = readmatrix(strcat('../data/real/', model,'/t_-cuda.txt'));
+    
+    err_opencl = norm(v_cuda - v_opencl)/norm(v_cuda) + norm(t_cuda - t_opencl)/norm(t_cuda);
     
     figure; 
-    subplot(1, 2, 1);
-    plot(y_cuda - y);
-    title('CUDA');
-    subplot(1, 2, 2);
-    plot(y_opencl - y);
-    title('OpenCL');
+    subplot(2, 2, 1);
+    plot(v_cuda - v);
+    title('CUDA (v)');
+    subplot(2, 2, 2);
+    plot(v_opencl - v);
+    title('OpenCL (v)');
+    subplot(2, 2, 3);
+    plot(t_cuda - t);
+    title('CUDA (t)');
+    subplot(2, 2, 4);
+    plot(t_opencl - t);
+    title('OpenCL (t)');
     sgtitle(strcat({'Model '}, upper(model), {', error = '}, num2str(err_opencl)));
 end
